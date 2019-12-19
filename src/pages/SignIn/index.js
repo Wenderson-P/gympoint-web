0300dfc4268eae75a6b4634ad80781331acc6218
Wenderson <wps713@gmail.com>
2019-12-19 14:20:24 -0300

    Add unform lib in sign-in

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 import logo from '~/assets/logo.svg';
 
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
+
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="" />
       <h1>GYMPOINT</h1>
-      <form>
+      <Form onSubmit={handleSubmit} schema={schema}>
         <label htmlFor="userEmail">SEU E-MAIL</label>
-        <input
+        <Input
           type="email"
           name="email"
           id="userEmail"
@@ -18,14 +29,14 @@ export default function SignIn() {
         />
 
         <label htmlFor="userPassword">SUA SENHA</label>
-        <input
+        <Input
           type="password"
           name="password"
           id="userPassword"
           placeholder="*******"
         />
         <button type="submit">Entrar no sistema</button>
-      </form>
+      </Form>
     </>
   );
 }
