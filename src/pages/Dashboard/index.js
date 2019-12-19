566f47f832ea2a2cf7eed6a75799e5d84e6c339a
Wenderson <wps713@gmail.com>
2019-12-19 10:57:25 -0300

    Setting up private routes

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h3>Dashboard</h3>
+    </div>
+  );
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from '~/routes/Routes';
 import SignIn from '~/pages/SignIn';
+import Dashboard from '~/pages/Dashboard';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
+      <Route path="/dashboard" exact component={Dashboard} isPrivate />
     </Switch>
   );
 }
