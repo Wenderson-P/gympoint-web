407beb1bd027a58e87010ec61ca797a53111d742
Wenderson <wps713@gmail.com>
2019-12-19 12:53:32 -0300

    Add sign in page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,31 @@
 import React from 'react';
 
 // import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="" />
+      <h1>GYMPOINT</h1>
+      <form>
+        <label htmlFor="userEmail">SEU E-MAIL</label>
+        <input
+          type="email"
+          name="email"
+          id="userEmail"
+          placeholder="exemplo@email.com"
+        />
+
+        <label htmlFor="userPassword">SUA SENHA</label>
+        <input
+          type="password"
+          name="password"
+          id="userPassword"
+          placeholder="*******"
+        />
+        <button type="submit">Entrar no sistema</button>
+      </form>
+    </>
+  );
 }
