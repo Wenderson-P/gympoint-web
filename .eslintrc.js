47fb3e05898c76da359ce732a426ba7e4ee96a42
Wenderson <wps713@gmail.com>
2019-12-19 11:58:24 -0300

    Add eslint rule to disable props spreading

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
     "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
     "import/prefer-default-export": "off",
     "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    'react/jsx-props-no-spreading': 'off',
     "react/jsx-one-expression-per-line": "off",
     "global-require": "off",
     "react-native/no-raw-text": "off",
