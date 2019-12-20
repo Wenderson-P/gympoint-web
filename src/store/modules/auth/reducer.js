311a21490793918adec8b34bd0a29fa47c114767
Wenderson <wps713@gmail.com>
2019-12-19 22:05:59 -0300

    Add auth reducer

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -1,7 +1,18 @@
-const initialState = {};
+import produce from 'immer';
 
-export default function auth(state = initialState, action) {
+const INITIAL_STATE = {
+  token: null,
+  signed: false,
+  loading: false,
+};
+
+export default function auth(state = INITIAL_STATE, action) {
   switch (action.type) {
+    case '@auth/SIGN_IN_SUCCESS':
+      return produce(state, draft => {
+        draft.token = action.payload.token;
+        draft.signed = true;
+      });
     default:
       return state;
   }
