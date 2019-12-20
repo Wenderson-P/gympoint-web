0b2ab00989ff47372b7c4079cd7d95ae268e81a1
Wenderson <wps713@gmail.com>
2019-12-20 20:30:16 -0300

    Changing homepage from dashboard to students

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -17,7 +17,7 @@ export default function RouteWrapper({
     return <Redirect to="/" />;
   }
   if (signed && !isPrivate) {
-    return <Redirect to="/dashboard" />;
+    return <Redirect to="/alunos" />;
   }
 
   const Layout = signed ? DefaultLayout : AuthLayout;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import Route from '~/routes/Routes';
 import SignIn from '~/pages/SignIn';
-import Dashboard from '~/pages/Dashboard';
+import Alunos from '~/pages/Alunos';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
-      <Route path="/dashboard" exact component={Dashboard} isPrivate />
+      <Route path="/alunos" exact component={Alunos} isPrivate />
     </Switch>
   );
 }
