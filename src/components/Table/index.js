7844b9112b678a9bf084ead31ee1bde97b669438
Wenderson <wps713@gmail.com>
2019-12-20 22:08:09 -0300

    Getting css from props and changing html

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,34 +1,42 @@
 import React from 'react';
-
-import { Container, Header } from './styles';
+import PropTypes from 'prop-types';
+import { TableContent, Header, Data, Button } from './styles';
 
 export default function Table({ headers, data, dataDisplay, options }) {
   return (
-    <Container>
-      <table>
-        <thead>
-          <tr>
-            {headers.map(header => (
-              <Header>{header}</Header>
-            ))}
-            <Header />
-          </tr>
-        </thead>
-        <tbody>
-          {data.map(item => (
-            <tr>
-              {dataDisplay.map(itemName => (
-                <td>{item[itemName]} </td>
-              ))}
-              {options.map(option => (
-                <button type="button" onClick={option.onclick}>
-                  {option.name}
-                </button>
-              ))}
-            </tr>
+    <TableContent>
+      <thead>
+        <tr>
+          {headers.map(header => (
+            <Header textAlign={header.textAlign}>{header.name}</Header>
           ))}
-        </tbody>
-      </table>
-    </Container>
+          <Header />
+        </tr>
+      </thead>
+      <tbody>
+        {data.map(item => (
+          <>
+            <tr style={{ borderBottom: '1px solid #000' }}>
+              {dataDisplay.map((itemName, index) => (
+                <Data textAlign={headers[index].textAlign}>
+                  {item[itemName]}
+                </Data>
+              ))}
+              <td>
+                {options.map(option => (
+                  <Button
+                    type="button"
+                    onClick={option.onclick}
+                    color={option.color}
+                  >
+                    {option.name}
+                  </Button>
+                ))}
+              </td>
+            </tr>
+          </>
+        ))}
+      </tbody>
+    </TableContent>
   );
 }
