1ad433378901d582439807a830bdefaf9df63bdd
Wenderson <wps713@gmail.com>
2019-12-20 22:08:15 -0300

    Changing table component style

--- src/components/Table/styles.js
+++ src/components/Table/styles.js
@@ -1,5 +1,45 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const TableContent = styled.table`
+  width: 100%;
+  margin-top: 20px;
+  background: #ffffff;
+  border-radius: 4px;
+  padding: 30px;
+  tbody {
+    tr {
+      height: 100%;
+      padding: 16px 0px;
+      border: 1px solid black;
+    }
+    td {
+      border-bottom: 1px solid #eeeeee;
+    }
+  }
+`;
 
-export const Header = styled.th``;
+export const Header = styled.th`
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 19px;
+  text-align: ${props => props.textAlign || 'left'};
+  color: #444444;
+  padding: 16px 0px;
+`;
+
+export const Data = styled.td`
+  font-size: 16px;
+  line-height: 20px;
+  text-align: ${props => props.textAlign || 'left'};
+  padding: 16px 0px;
+  color: #666666;
+`;
+
+export const Button = styled.button`
+  background: none !important ;
+  border: none !important ;
+  margin-left: 20px !important ;
+  color: ${props => props.color || '#4D85EE'} !important ;
+  padding: 0px !important;
+  font-weight: normal !important;
+`;
