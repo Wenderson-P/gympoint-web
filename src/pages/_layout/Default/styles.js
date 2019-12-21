f7a7f9a476edd2e2907dc5727fdb4effaa78c6d2
Wenderson <wps713@gmail.com>
2019-12-20 22:11:22 -0300

    Add default page layout style

--- src/pages/_layout/Default/styles.js
+++ src/pages/_layout/Default/styles.js
@@ -4,3 +4,20 @@ export const Wrapper = styled.div`
   height: 100%;
   background: #f5f5f5;
 `;
+
+export const Content = styled.div`
+  padding: 30px;
+  width: 90%;
+  margin: 0 auto;
+  button {
+    background: #ee4d64;
+    color: #ffffff;
+    border: none;
+    border-radius: 4px;
+    padding: 10px;
+    font-weight: bold;
+    font-size: 15px;
+    line-height: 16px;
+    text-align: center;
+  }
+`;
