1c7a340f58c92123919a9843eddba62140f5497f
Wenderson <wps713@gmail.com>
2019-12-20 22:11:31 -0300

    Add style in students page

---
+++ src/pages/Students/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 90%;
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
