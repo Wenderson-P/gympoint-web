897db1d2478f8c3cd70bd7ab38316c63285dc0aa
Wenderson <wps713@gmail.com>
2019-12-20 22:36:41 -0300

    Add plans route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,14 @@ import { Switch } from 'react-router-dom';
 import Route from '~/routes/Routes';
 import SignIn from '~/pages/SignIn';
 import Students from '~/pages/Students';
+import Plans from '~/pages/Plans';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/alunos" exact component={Students} isPrivate />
+      <Route path="/plans" exact component={Plans} isPrivate />
     </Switch>
   );
 }
