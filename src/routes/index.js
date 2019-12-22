54f178547c4c174bb9399fc7104dac0c7dd62275
Wenderson <wps713@gmail.com>
2019-12-22 14:12:11 -0300

    Changing routes /edit to /form

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import SignIn from '~/pages/SignIn';
 import Students from '~/pages/Students';
 import StudentForm from '~/pages/Students/StudentForm';
 import Plans from '~/pages/Plans';
+import PlanForm from '~/pages/Plans/PlanForm';
 import Enrollments from '~/pages/Enrollments';
 import HelpOrders from '~/pages/HelpOrders';
 
@@ -13,8 +14,9 @@ export default function Routes() {
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/students" exact component={Students} isPrivate />
-      <Route path="/students/edit/:name" component={StudentForm} isPrivate />
+      <Route path="/students/form" component={StudentForm} isPrivate />
       <Route path="/plans" exact component={Plans} isPrivate />
+      <Route path="/plans/form" exact component={PlanForm} isPrivate />
       <Route path="/enrollments" exact component={Enrollments} isPrivate />
       <Route path="/help-orders" exact component={HelpOrders} isPrivate />
     </Switch>
