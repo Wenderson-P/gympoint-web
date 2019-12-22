3c99f105273188d55d9767ed17b79303d88127fa
Wenderson <wps713@gmail.com>
2019-12-22 01:22:27 -0300

    Add default style for headers and form components

--- src/pages/_layout/Default/styles.js
+++ src/pages/_layout/Default/styles.js
@@ -19,8 +19,53 @@ export const Content = styled.div`
       font-size: 24px;
       color: #444444;
     }
-  }
 
+    margin-bottom: 20px;
+    button {
+      display: flex;
+      align-items: center;
+      margin-left: 16px;
+      svg {
+        margin: 0px 8px;
+      }
+      &:first-child {
+        background: #cccccc;
+      }
+    }
+  }
+  form {
+    padding: 30px;
+    display: flex;
+    flex-direction: column;
+    background: #ffffff;
+    border-radius: 4px;
+    div {
+      display: flex;
+      flex-wrap: wrap;
+    }
+    label {
+      display: flex;
+
+      flex-direction: column;
+      font-weight: bold;
+
+      font-size: 14px;
+      color: #444444;
+      text-align: left;
+    }
+
+    input {
+      margin: 8px 15px;
+      padding: 13px 10px;
+      font-size: 16px;
+      color: #666666;
+      text-align: left;
+
+      border: 1px solid #dddddd;
+      box-sizing: border-box;
+      border-radius: 4px;
+    }
+  }
   button {
     background: #ee4d64;
     color: #ffffff;
