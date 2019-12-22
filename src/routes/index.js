f90070af0277764e2613748b243d9d2c4f8c638b
Wenderson <wps713@gmail.com>
2019-12-22 01:13:09 -0300

    Add route to students edit form

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { Switch } from 'react-router-dom';
 import Route from '~/routes/Routes';
 import SignIn from '~/pages/SignIn';
 import Students from '~/pages/Students';
+import StudentForm from '~/pages/Students/StudentForm';
 import Plans from '~/pages/Plans';
 import Enrollments from '~/pages/Enrollments';
 import HelpOrders from '~/pages/HelpOrders';
@@ -12,6 +13,7 @@ export default function Routes() {
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/students" exact component={Students} isPrivate />
+      <Route path="/students/edit/:name" component={StudentForm} isPrivate />
       <Route path="/plans" exact component={Plans} isPrivate />
       <Route path="/enrollments" exact component={Enrollments} isPrivate />
       <Route path="/help-orders" exact component={HelpOrders} isPrivate />
