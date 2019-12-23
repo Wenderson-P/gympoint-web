aff7c48c597270cc3ab25d8d7de8574447aa286a
Wenderson <wps713@gmail.com>
2019-12-23 14:07:14 -0300

    Add border 0 in global style

--- src/styles/global.js
+++ src/styles/global.js
@@ -4,8 +4,9 @@ export default createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
 
   *{
-    margin:0;
-    padding:0;
+    border: 0;
+    padding: 0;
+    margin: 0;
     box-sizing:border-box;
   }
 
