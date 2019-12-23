bca166d65062188201354bab483970a258ebb4f0
Wenderson <wps713@gmail.com>
2019-12-23 14:00:33 -0300

    Add pageHeader Component

---
+++ src/components/PageHeader/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FaPlus } from 'react-icons/fa';
+
+import { Container } from './styles';
+
+export default function PageHeader({ pageName, history }) {
+  function handleAddClick() {
+    const { pathname } = history.location;
+
+    history.push(`${pathname}/form`);
+  }
+
+  return (
+    <Container>
+      <h2>{pageName}</h2>
+      <button type="button" onClick={handleAddClick}>
+        <FaPlus /> CADASTRAR
+      </button>
+    </Container>
+  );
+}
