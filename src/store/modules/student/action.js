7526fef9ff6499243979964293a4b90491c2c43b
Wenderson <wps713@gmail.com>
2019-12-23 14:01:03 -0300

    Add student redux actions on store & update

---
+++ src/store/modules/student/action.js
@@ -0,0 +1,37 @@
+export function storeRequest(name, email, weight, height, age) {
+  return {
+    type: '@student/STORE_REQUEST',
+    payload: { name, email, weight, height, age },
+  };
+}
+
+export function storeSuccess(student) {
+  return {
+    type: '@student/STORE_SUCCESS',
+    payload: { student },
+  };
+}
+export function storeFailure() {
+  return {
+    type: '@student/STORE_FAILURE',
+  };
+}
+
+export function updateRequest(name, email, weight, height, age) {
+  return {
+    type: '@student/UPDATE_REQUEST',
+    payload: { name, email, weight, height, age },
+  };
+}
+
+export function updateSuccess(student) {
+  return {
+    type: '@student/UPDATE_SUCCESS',
+    payload: { student },
+  };
+}
+export function updateFailure(student) {
+  return {
+    type: '@student/UPDATE_FAILURE',
+  };
+}
