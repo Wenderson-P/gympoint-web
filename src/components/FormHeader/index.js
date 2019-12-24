fc9daead743a0e5ac19c64080aa4559c776c7686
Wenderson <wps713@gmail.com>
2019-12-23 22:41:51 -0300

    Add formHeader component

---
+++ src/components/FormHeader/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FaChevronLeft, FaCheck } from 'react-icons/fa';
+
+// import { Container } from './styles';
+
+export default function FormHeader({ formType, addTitle, editTitle, history }) {
+  function goBack() {
+    history.goBack();
+  }
+  return (
+    <div>
+      <h2>{formType === 'add' ? `${addTitle}` : `${editTitle}`}</h2>
+      <aside>
+        <button type="button" onClick={goBack}>
+          <FaChevronLeft size={14} />
+          Voltar
+        </button>
+        <button type="submit">
+          <FaCheck size={14} />
+          Salvar
+        </button>
+      </aside>
+    </div>
+  );
+}
