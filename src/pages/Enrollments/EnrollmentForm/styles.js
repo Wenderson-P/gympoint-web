9f2a07dfd0eb94b48bc6309a6c93e8736c77a0e1
Wenderson <wps713@gmail.com>
2019-12-26 16:25:47 -0300

    Add enrollment form page style

---
+++ src/pages/Enrollments/EnrollmentForm/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Row = styled.fieldset`
+  input {
+    width: 100%;
+  }
+`;
+
+export const MultipleItemRow = styled.fieldset`
+  input {
+    width: 15%;
+  }
+`;
