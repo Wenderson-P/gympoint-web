0ff7fea4ed7367791bbf75d1883cc82b0dcfff55
Wenderson <wps713@gmail.com>
2019-12-27 16:01:42 -0300

    Add student form style

--- src/pages/Students/StudentForm/styles.js
+++ src/pages/Students/StudentForm/styles.js
@@ -1,9 +1,12 @@
 import styled from 'styled-components';
 
-export const Row = styled.div`
-  label {
-    width: 100%;
+export const Row = styled.fieldset`
+  display: flex;
+  input {
+    width: 95%;
   }
 `;
 
-export const MultipleItemRow = styled.div``;
+export const MultipleItemRow = styled.fieldset``;
+
+export const FormBody = styled.div``;
