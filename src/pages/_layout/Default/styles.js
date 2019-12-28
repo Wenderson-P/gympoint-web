9c07c944a558aff7045fd802f1ecdca6e4824661
Wenderson <wps713@gmail.com>
2019-12-28 16:38:22 -0300

    Changing default app layout style

--- src/pages/_layout/Default/styles.js
+++ src/pages/_layout/Default/styles.js
@@ -28,56 +28,27 @@ export const Content = styled.div`
   }
 
   form {
-    padding: 30px;
-    div {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      margin-bottom: 20px;
-      aside {
-        display: flex;
-
-        button {
-          display: flex;
-          align-items: center;
-          margin-left: 16px;
-          svg {
-            margin: 0px 8px;
-          }
-          &:first-child {
-            background: #cccccc;
-          }
-        }
-      }
-    }
-
     div:last-child {
-      display: flex;
-      flex-direction: column;
       background: #ffffff;
       border-radius: 4px;
-      fieldset {
-        padding: 8px 15px;
-        width: 100%;
 
-        label {
-          font-weight: bold;
-          font-size: 14px;
-          color: #444444;
-          text-align: left;
-        }
+      label {
+        font-weight: bold;
+        font-size: 14px;
+        color: #444444;
+        text-align: left;
+      }
 
-        input {
-          margin: 8px;
-          padding: 13px 10px;
-          font-size: 16px;
-          color: #666666;
-          text-align: left;
+      input {
+        margin: 8px;
+        padding: 13px 10px;
+        font-size: 16px;
+        color: #666666;
+        text-align: left;
 
-          border: 1px solid #dddddd;
-          box-sizing: border-box;
-          border-radius: 4px;
-        }
+        border: 1px solid #dddddd;
+        box-sizing: border-box;
+        border-radius: 4px;
       }
     }
   }
