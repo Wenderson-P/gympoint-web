e099e497e0bacf87d824f43b5b5c38fe2ef723c9
Wenderson <wps713@gmail.com>
2020-01-09 11:48:08 -0300

    Add Page header & SearchBar style

---
+++ src/pages/Students/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const PageHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+export const SearchBar = styled.input`
+  border-radius: 4px;
+  margin-left: 10px;
+  padding: 10px;
+  font-size: 15px;
+`;
